dd0be8d23f3d9fe3c7161ac3c758b95b6b64ad5f
eiroca <39483731+eiroca@users.noreply.github.com>
2019-07-26 16:02:31 +0200

    copyright update

--- DiskCheck/DiskCheck.cs
+++ DiskCheck/DiskCheck.cs
@@ -1,6 +1,6 @@
 ﻿/**
  *
- * Copyright (C) 2001-2019 eIrOcA (eNrIcO Croce & sImOnA Burzio) - AGPL >= 3.0
+ * Copyright (C) 2001-2019 Enrico Croce - AGPL >= 3.0
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
